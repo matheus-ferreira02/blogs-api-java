f01298ae57dd28337548e029ab7ac461e0f82eb1
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-10-23 16:55:53 -0300

    feat: iniciando o projeto

---
+++ src/main/java/com/blogsapijava/BlogsApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.blogsapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsApiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsApiJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blogsapijava/BlogsApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogsapijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsApiJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
