bd4d9c6739c04f0917d2c1a5e6b959d2d5c5956c
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-10-25 12:17:35 -0300

    feat: UserRequestDTO created

---
+++ src/main/java/com/blogsapijava/dtos/UserRequestDTO.java
@@ -0,0 +1,24 @@
+package com.blogsapijava.dtos;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class UserRequestDTO {
+
+    @NotNull(message = "displayName is required")
+    @Size(max = 50, message = "Maximum length 0f 50 characters reached")
+    private String displaName;
+
+    @NotNull(message = "password is required")
+    @Size(max = 50, message = "Maximum length 0f 50 characters reached")
+    private String password;
+
+    @NotNull(message = "email is required")
+    @Size(max = 200, message = "Maximum length 0f 200 characters reached")
+    @Email(message = "Email is not valid")
+    private String email;
+
+    @Size(max = 255, message = "Maximum length 0f 255 characters reached")
+    private String image;
+}
