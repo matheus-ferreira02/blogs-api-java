0da9daf7e2e98a33ff0675dfe78f7fd5de78c221
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-10-26 18:20:15 -0300

    feat: created category service interface

--- src/main/java/com/blogsapijava/interfaces/ICategoryService.java
+++ src/main/java/com/blogsapijava/interfaces/ICategoryService.java
@@ -1,2 +1,13 @@
-package com.blogsapijava.interfaces;public interface ICategoryService {
+package com.blogsapijava.interfaces;
+
+import com.blogsapijava.models.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category findById(long id);
+    Category findByName(String name);
+    Category create(Category category);
+    List<Category> findAll();
+    void delete(long id);
 }
